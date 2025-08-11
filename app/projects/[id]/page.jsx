4ce99bbc8a358227536643c7d26e45ce17b08eb8
Keyvan Mahani <167319451+KevinMahani@users.noreply.github.com>
2025-08-11 20:41:00 +0330

    add zarmatn project

--- app/projects/[id]/page.jsx
+++ app/projects/[id]/page.jsx
@@ -1,20 +1,20 @@
-
 "use client";
 
-
+import { useParams } from "next/navigation";
 import projectsEN from "../../data/projects";
 import projectsDe from "../../data/projects-de";
-import { notFound } from "next/navigation";
 import MyPages from "../../components/MyPages";
 import { useLanguage } from "../../context/LanguageContext";
 
-
-export default function ProjectPage({ params }) {
+export default function ProjectPage() {
+  const params = useParams();
   const { lang } = useLanguage();
   const projects = lang === "de" ? projectsDe : projectsEN;
 
   const project = projects.find((p) => p.id.toString() === params.id);
-  if (!project) return notFound();
+  if (!project) {
+    return <div>Project not found</div>;
+  }
 
   return <MyPages params={params} project={project} />;
 }
